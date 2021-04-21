e86e768f14666b06257fe1060de6775656bfc96a
Giulio Moro <giuliomoro@yahoo.it>
2021-04-21 13:50:35 +0000

    Linux with i2c-dev without smbus

--- sys/linux-i2c/common/linux-i2c.c
+++ sys/linux-i2c/common/linux-i2c.c
@@ -1,8 +1,6 @@
 #include <errno.h>
 #include <fcntl.h>
 #include <linux/i2c-dev.h>
-#include <linux/i2c-dev.h>
-#include <i2c/smbus.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <string.h>
@@ -68,7 +66,8 @@ u8x8_byte_linux_i2c(u8x8_t *u8x8,
 	case U8X8_MSG_BYTE_END_TRANSFER:
 		//fprintf(stderr, "++ end transfer, sending cmd %0x %0x count %d\n", data[0], data[1], idx);
 		// NB! note the extre _i2c_ in there! leave that out and you are screwed
-		if (i2c_smbus_write_i2c_block_data(file, data[0], idx - 1, &data[1]) < 0) {
+		if (write(file, data, idx) != idx) {
+		//if (i2c_smbus_write_i2c_block_data(file, data[0], idx - 1, &data[1]) < 0) {
 			fprintf(stderr, "can't write cmd %0x: %s\n",  data[0], strerror(errno));
 			return(errno); 
 		}
